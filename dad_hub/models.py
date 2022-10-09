d39ef98c3c23d84fb71a7f0e781e0a0517bf3eb8
ekelly55 <eamonn.michael.kellyr@email.com>
2022-10-09 12:25:34 -0400

    blurb displays username and content on adminpage. all blurbs showing up on postgres

--- dad_hub/models.py
+++ dad_hub/models.py
@@ -5,11 +5,10 @@ from django.contrib.auth.models import User
 # we'll need blurbs, profile, response
 
 class Blurb(models.Model):
-    name = models.CharField(max_length=250)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     content = models.TextField(max_length=1000)
     created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.user, self.content
 
